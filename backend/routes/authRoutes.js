afd02fe077b9fe206c2f0ede23388ebb85cf1132
Ojo Mu'adh <ojomuadh9@gmail.com>
2026-06-01 18:05:15 +0100

    all error fixed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { register, login } from "../controllers/authController.js";
+import authController from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 import roleMiddleware from "../middleware/roleMiddleware.js";
 
 
 
-router.post("/register", authMiddleware, roleMiddleware("admin"), register);
-router.post("/login",authMiddleware, login);
+router.post("/register", authMiddleware, roleMiddleware("admin"), authController.register);
+router.post("/login", authMiddleware, authController.login);
 
 
 export default router;
