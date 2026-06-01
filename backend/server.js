b4c9ab64ce8d08f5add7533f73f10be0eef3f429
Ojo Mu'adh <ojomuadh9@gmail.com>
2026-06-01 17:57:45 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import connectDB from './config/db.js';
-import authController from "./controllers/authController.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import moduleRoutes from "./routes/moduleRoutes.js";
+import assignmentRoutes from "./routes/assignmentRoutes.js";
+import progressRoutes from "./routes/progressRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
 
 dotenv.config();
 
@@ -16,12 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api/auth',        require('./routes/authRoutes.js'));
-app.use('/api/users',       require('./routes/userRoutes.js'));
-app.use('/api/modules',     require('./routes/moduleRoutes.js'));
-app.use('/api/assignments', require('./routes/assignmentRoutes.js'));
-app.use('/api/progress',    require('./routes/progressRoutes.js'));
-app.use('/api/analytics',   require('./routes/analyticsRoutes.js'));
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/modules', moduleRoutes);
+app.use('/api/assignments', assignmentRoutes);
+app.use('/api/progress',    progressRoutes);
+app.use('/api/analytics',   analyticsRoutes);
 // Health check
 app.get('/api/health', (req, res) => res.json({ status: 'ok' }));
 
