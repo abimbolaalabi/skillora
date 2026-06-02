ab062a1bbf782c43ead4ae730070742ed8adea9b
johnsonjrf <johnsonoyetunji@gmail.com>
2026-06-03 01:48:32 +0300

    added id params to the module update route

--- backend/routes/moduleRoutes.js
+++ backend/routes/moduleRoutes.js
@@ -6,7 +6,7 @@ const {createModule, getAllModule, getModuleById, updateModule, deleteModule, pu
 // TODO: wire up module controller methods
 router.get("/getAllModule", getAllModule);
 router.get("/getModuleById/:id", getModuleById);
-router.patch("/updateModule", updateModule);
+router.patch("/updateModule/:id", updateModule);
 router.patch("/publishModule/:id", publishModule);
 router.post("/createModule", upload.single("video"), createModule);
 router.delete("/deleteModule/:id", deleteModule);
