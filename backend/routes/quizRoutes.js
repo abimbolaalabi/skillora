55d1474219a95bce4d91cbcc2ea57a29c8b6da40
Ojo Mu'adh <ojomuadh9@gmail.com>
2026-06-11 13:52:11 +0100

    final adjustment

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import authMiddleware from '../middleware/authMiddleware.js';
 import { submitQuiz, getQuizScore} from '../controllers/quizController.js';
 
 const router = express.Router();
 
-router.post('/:quizId/submit/:userId', submitQuiz);
-router.get('/:quizId/score/:userId', getQuizScore);
+router.post('/:quizId/submit/:userId', authMiddleware, submitQuiz);
+router.get('/:quizId/score/:userId', authMiddleware, getQuizScore);
 
 
 
